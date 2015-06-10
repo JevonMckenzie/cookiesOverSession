455ed3b046c62bd7cd72832fa0b7131e47c0fe08
JevonMckenzie <jevonmckenzie.wesleycollege@gmail.com>
2015-06-11 01:25:24 +0800

    removed references to creditcard.rb api

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require 'json'
 require 'sinatra'
-require_relative 'model/credit_card.rb'
+#require_relative 'model/credit_card.rb'
 require_relative 'model/user.rb'
 require_relative 'helpers/credit_card_api_helper.rb'
 require 'rack-flash'
@@ -28,7 +28,7 @@ class CreditCardAPI < Sinatra::Base
   configure :production do
     use Rack::SslEnforcer
     set :session_secret, ENV['MSG_KEY']
-    
+
   end
 
 
@@ -112,55 +112,4 @@ class CreditCardAPI < Sinatra::Base
     redirect '/'
   end
 
-  get '/api/v1/credit_card/validate/?' do
-    number = params[:card_number]
-    halt 400 unless number
-    card = CreditCard.new
-    card.number = number
-    {
-      card: number,
-      validated: card.validate_checksum
-    }.to_json
-  end
-
-  post '/api/v1/credit_card/?' do
-    request_json = request.body.read
-    unless request_json.empty?
-      begin
-        obj = JSON.parse(request_json)
-        card = CreditCard.new(
-          expiration_date: obj['expiration_date'],
-          owner: obj['owner'],
-          credit_network: obj['credit_network']
-        )
-        card.number = obj['number'].chomp
-        if card.validate_checksum && card.save
-          status 201
-          body({
-            status: 201,
-            message: 'Created'
-          }.to_json)
-        else
-          status 410
-          body({
-            status: 410,
-            message: 'Gone'
-          }.to_json)
-        end
-      rescue
-        halt 400, {
-          status: 400,
-          message: 'Bad Request'
-        }.to_json
-      end
-    end
-  end
-
-  get '/api/v1/credit_card/all/?' do
-    begin
-      CreditCard.all.map(&:to_hash).to_json
-    rescue
-      halt 500
-    end
-  end
 end
